7ad18f55b36a613ed33208d26d416f1cb4ca9420
laurahw <lhanna-white@andigital.com>
2018-08-14 16:02:40 +0100

    :sparkles: Adding routing between solutions

--- src/App.js
+++ src/App.js
@@ -1,47 +1,35 @@
 import React, { Component } from 'react'
 import './App.css'
-import product from './images/shoes.jpg'
-import suggestion1 from './images/shoes1.png'
-import suggestion2 from './images/shoes2.png'
+// import product from './images/shoes.jpg'
+// import suggestion1 from './images/shoes1.png'
+// import suggestion2 from './images/shoes2.png'
+import Solution1 from './Solution1'
+import Solution2 from './Solution2'
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header">
-        <h1 className="title">Sole Destroying Shoes, Ltd.</h1>
-        </header>
-        <div className="body">
-          <div className="product-container">
-            <img src={product} />
-            <div className="text">
-              <h2>Terrible Shoes</h2>
-              rrp: £1,050
-              <span>now only £500!</span>
-              <div className="button">
-              <span>Add to cart</span>
-              </div>
-            </div>
-          </div>
-          <div className="suggestions-container">
-            <h2>You may also like...</h2>
-          <div className="suggestion">
-            <img src={suggestion1} />
-            <h3>Hairbrush Fluff on a shoe</h3>
-            <div className="button">
-            <span>See more</span>
-            </div>
-          </div>
+        <Router>
+      <div>
+     <ul>
+       <li>
+         <Link to="/solution1">Solution 1</Link>
+       </li>
+       <li>
+         <Link to="/solution2">Solution 2</Link>
+       </li>
+     </ul>
 
-          <div className="suggestion">
-            <img src={suggestion2} />
-          <h3>Croc of Shit</h3>
-            <div className="button">
-            <span>See more</span>
-            </div>
-          </div>
-          </div>
-        </div>
+     <hr />
+
+    <Route path="/solution1" component={Solution1} />
+    <Route path="/solution2" component={Solution2} />
+   </div>
+ </Router>
+
       </div>
     );
   }
---
+++ src/Solution1.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import './App.css'
+import product from './images/shoes.jpg'
+import suggestion1 from './images/shoes1.png'
+import suggestion2 from './images/shoes2.png'
+
+class Solution1 extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="header">
+        <h1 className="title">Sole Destroying Shoes, Ltd.</h1>
+        </header>
+        <div className="body">
+          <div className="product-container">
+            <img src={product} />
+            <div className="text">
+              <h2>Terrible Shoes</h2>
+              rrp: £1,050
+              <span>now only £500!</span>
+              <div className="button">
+              <span>Add to cart</span>
+              </div>
+            </div>
+          </div>
+          <div className="suggestions-container">
+            <h2>You may also like...</h2>
+          <div className="suggestion">
+            <img src={suggestion1} />
+            <h3>Hairbrush Fluff on a shoe</h3>
+            <div className="button">
+            <span>See more</span>
+            </div>
+          </div>
+
+          <div className="suggestion">
+            <img src={suggestion2} />
+          <h3>Croc of Shit</h3>
+            <div className="button">
+            <span>See more</span>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Solution1;
